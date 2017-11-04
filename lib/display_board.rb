08c43591d9679882e6da28c878440bda09347fbd
Julio Torres <jtorres1989@gmail.com>
2017-11-04 17:02:21 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,6 +8,7 @@ def display_board(board)
        "-----------",
        "   |   |   "
 
-  "#{board[0]}, #{board[1]}, #{board[2]}, #{board[3]}, #{board[4]},
-  #{board[5]}, #{board[6]}, #{board[7]}, #{board[8]}"
+  puts "#{board[0]}, #{board[1]}, #{board[2]}"
+  puts "#{board[3]}, #{board[4]}, #{board[5]}"
+  puts "#{board[6]}, #{board[7]}, #{board[8]}"
 end
